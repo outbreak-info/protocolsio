0ec04aba6558273174b3334168e3d6fbcd5b86e2
Marco <artofmarco@gmail.com>
2020-07-10 08:51:06 -0700

    add schedule

--- dump.py
+++ dump.py
@@ -26,3 +26,5 @@ class PDumper(biothings.hub.dataload.dumper.DummyDumper):
     }
     # override in subclass accordingly
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+
+    SCHEDULE = "20 20 * * *"
