5f7b8e171e20a1edbb1f19253f2bb51d839a148a
juliamullen <julia.l.mullen@gmail.com>
2020-10-11 13:11:12 -0700

    protocolsio dump release version

--- dump.py
+++ dump.py
@@ -5,6 +5,7 @@ biothings.config_for_app(config)
 from config import DATA_ARCHIVE_ROOT
 
 import biothings.hub.dataload.dumper
+import datetime
 
 
 class PDumper(biothings.hub.dataload.dumper.DummyDumper):
@@ -28,3 +29,10 @@ class PDumper(biothings.hub.dataload.dumper.DummyDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
 
     SCHEDULE = "20 6 * * *"
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_release()
+
+    def set_release(self):
+        self.release = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M')
